2279510fc9bb16e68f13f511d548e08e30bcd03b
Brian Lego <legobw@gmail.com>
2020-06-19 13:49:06 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -4,6 +4,5 @@
 def meal_choice (veg1, veg2, protein="tofu")
   puts "What a nutritious meal!"
   message_b = "A plate of #{protein} with #{veg1} and #{veg2}."
-  return "What a nutritious meal!"
-  return message_b
+  return "What a nutritious meal!" message_b
 end
